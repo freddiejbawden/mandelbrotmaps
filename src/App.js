e76823b4e07d83eba15e975031d351f52311d616
freddiejbawden <freddiejbawden@gmail.com>
2019-10-10 15:13:54 +0100

    WASM Hello world

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React, { useState, useEffect }  from 'react';
 import './App.css';
 import Mandelbrot from './Mandelbrot/Mandelbrot'
 class App extends React.Component {
-  
+  componentDidMount() {
+    this.loadWasm();
+  }
+  loadWasm = async () => {
+    try {
+      const wasm = await import('mmap');
+      this.setState({wasm});
+      wasm.greet();
+    } catch(err) {
+      console.error(`Unexpected error in loadWasm. [Message: ${err.message}]`);
+    }
+  };
   render() {
     return (
       <div className="App">
