211eecdeeb95d6c1a6f9778f3adfc9a189e85c46
freddiejbawden <freddiejbawden@gmail.com>
2019-11-01 14:18:29 +0000

    track foo@1.2.3 in package.json

--- wasm/pkg/mmap.js
+++ wasm/pkg/mmap.js
@@ -31,42 +31,21 @@ export class Mandelbrot {
         wasm.__wbg_mandelbrot_free(ptr);
     }
     /**
-    * @param {number} max_i
     */
-    set_max_i(max_i) {
-        wasm.mandelbrot_set_max_i(this.ptr, max_i);
+    static greet() {
+        wasm.mandelbrot_greet();
     }
     /**
     * @param {number} width
     * @param {number} height
-    */
-    set_width_height(width, height) {
-        wasm.mandelbrot_set_width_height(this.ptr, width, height);
-    }
-    /**
     * @param {number} fractal_limit_x
     * @param {number} fractal_limit_y
-    */
-    set_limits(fractal_limit_x, fractal_limit_y) {
-        wasm.mandelbrot_set_limits(this.ptr, fractal_limit_x, fractal_limit_y);
-    }
-    /**
-    * @param {number} pixel_size
-    */
-    set_pixel_size(pixel_size) {
-        wasm.mandelbrot_set_pixel_size(this.ptr, pixel_size);
-    }
-    /**
-    * @param {number} width
-    * @param {number} height
     * @param {number} pixel_size
     * @param {number} max_i
-    * @param {number} centre_coords_x
-    * @param {number} centre_coords_y
     * @returns {Mandelbrot}
     */
-    static new(width, height, pixel_size, max_i, centre_coords_x, centre_coords_y) {
-        const ret = wasm.mandelbrot_new(width, height, pixel_size, max_i, centre_coords_x, centre_coords_y);
+    static new(width, height, fractal_limit_x, fractal_limit_y, pixel_size, max_i) {
+        const ret = wasm.mandelbrot_new(width, height, fractal_limit_x, fractal_limit_y, pixel_size, max_i);
         return Mandelbrot.__wrap(ret);
     }
     /**
@@ -75,16 +54,13 @@ export class Mandelbrot {
     */
     escape_algorithm(pixel_num) {
         const ret = wasm.mandelbrot_escape_algorithm(this.ptr, pixel_num);
-        return ret >>> 0;
-    }
-    /**
-    * @returns {number}
-    */
-    render() {
-        const ret = wasm.mandelbrot_render(this.ptr);
         return ret;
     }
 }
+
+export const __wbg_alert_c992628627e92c20 = function(arg0, arg1) {
+    alert(getStringFromWasm(arg0, arg1));
+};
 
 export const __wbindgen_throw = function(arg0, arg1) {
     throw new Error(getStringFromWasm(arg0, arg1));
