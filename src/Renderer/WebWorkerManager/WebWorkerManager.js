c30a4e75c7e162612c2f1fa7aa3bb4f9c8e3eacc
freddiejbawden <freddiejbawden@gmail.com>
2020-01-31 17:21:03 +0000

    Render Chunks

--- src/Renderer/WebWorkerManager/WebWorkerManager.js
+++ src/Renderer/WebWorkerManager/WebWorkerManager.js
@@ -3,7 +3,7 @@ import idGenerator from '../../utils/IDGenerator';
 
 class WebWorkerManager {
   constructor(type) {
-    this.remaining_threads = 0;
+    this.remaining_chunks = 0;
     this.workers = [];
     this.type = type;
   }
@@ -19,42 +19,28 @@ class WebWorkerManager {
     renderer,
   ) {
     return new Promise((res) => {
+      this.nextChunk = 0;
       this.arr = new Uint8ClampedArray(height * width * 4);
       const hardwareConcurrency = navigator.hardwareConcurrency || 1;
       const nThreadsFree = (singleThread) ? 1 : hardwareConcurrency;
-      this.pixelSplit = (height * width) / nThreadsFree;
-      this.remaining_threads = nThreadsFree;
+      const nChunks = (singleThread) ? 1 : 10;
+      this.pixelSplit = (height * width) / nChunks;
+      this.remaining_chunks = nChunks;
       const roundID = idGenerator();
-      if (this.workers.length < this.remaining_threads) {
-        for (let i = this.workers.length; i < this.remaining_threads; i += 1) {
+      if (this.workers.length < nThreadsFree) {
+        for (let i = this.workers.length; i < nThreadsFree; i += 1) {
           const w = new Worker('./worker/renderworker.js', { name: 'w', type: 'module' });
           this.workers.push(w);
         }
       }
-      for (let i = 0; i < this.remaining_threads; i += 1) {
-        const w = this.workers[i];
-        w.onmessage = (e) => {
-          if (e.data.id === roundID) {
-            this.arr.set(e.data.arr, e.data.offset);
-            this.remaining_threads -= 1;
-            if (this.remaining_threads === 0) {
-              res(
-                {
-                  arr: this.arr,
-                  width,
-                  height,
-                },
-              );
-            }
-          }
-        };
+      const renderChunk = (w, startPixel, endPixel) => {
         w.postMessage({
           type: this.type,
           id: roundID,
           renderer,
-          startPixel: Math.floor(i * this.pixelSplit),
-          endPixel: Math.floor((i + 1) * this.pixelSplit),
-          arrSize: this.pixelSplit * 4,
+          startPixel,
+          endPixel,
+          arrSize: Math.floor((endPixel - startPixel) * 4),
           pixelSize,
           width,
           height,
@@ -62,6 +48,37 @@ class WebWorkerManager {
           centreCoords,
           juliaPoint,
         });
+        this.nextChunk += 1;
+      };
+
+      for (let i = 0; i < nThreadsFree; i += 1) {
+        const w = this.workers[i];
+        w.onmessage = (e) => {
+          if (e.data.id === roundID) {
+            this.arr.set(e.data.arr, e.data.offset);
+            this.remaining_chunks -= 1;
+            if (this.remaining_chunks === 0) {
+              res(
+                {
+                  arr: this.arr,
+                  width,
+                  height,
+                },
+              );
+            } else if (this.remaining_chunks >= nThreadsFree) {
+              renderChunk(
+                w,
+                Math.floor((this.nextChunk) * this.pixelSplit),
+                Math.floor((this.nextChunk + 1) * this.pixelSplit),
+              );
+            }
+          }
+        };
+        renderChunk(
+          w,
+          Math.floor((this.nextChunk) * this.pixelSplit),
+          Math.floor((this.nextChunk + 1) * this.pixelSplit),
+        );
       }
     });
   }
@@ -82,12 +99,14 @@ class WebWorkerManager {
     renderer,
   ) {
     return new Promise((res) => {
+      this.nextChunk = 0;
       this.width = width;
       this.height = height;
       const newArr = new Uint8ClampedArray(height * width * 4);
       const nThreadsFree = (singleThread) ? 1 : navigator.hardwareConcurrency;
-      this.pixelSplit = height / nThreadsFree;
-      this.remaining_threads = nThreadsFree;
+      const nChunks = 4;
+      this.pixelSplit = height / nChunks;
+      this.remaining_chunks = nChunks;
       const roundID = idGenerator();
       if (this.workers.length < nThreadsFree) {
         for (let i = this.workers.length; i < nThreadsFree; i += 1) {
@@ -95,37 +114,15 @@ class WebWorkerManager {
           this.workers.push(w);
         }
       }
-      for (let i = 0; i < nThreadsFree; i += 1) {
-        const w = this.workers[i];
-        w.onmessage = (e) => {
-          if (e.data.id === roundID) {
-            try {
-              newArr.set(e.data.fractal.arr, e.data.offset);
-            } catch (err) {
-              // eslint-disable-next-line no-console
-              console.error(err);
-            }
-            this.remaining_threads -= 1;
-            if (this.remaining_threads === 0) {
-              this.arr = newArr;
-              res(
-                {
-                  arr: this.arr,
-                  height: this.height,
-                  width: this.width,
-                },
-              );
-            }
-          }
-        };
+      const renderChunk = (w, startRow, endRow) => {
         w.postMessage({
           type: this.type,
           id: roundID,
           workerID: idGenerator(),
           renderer,
           mode: 'partial',
-          startRow: Math.floor(i * this.pixelSplit),
-          endRow: Math.floor((i + 1) * this.pixelSplit),
+          startRow,
+          endRow,
           pixelSize,
           width,
           height,
@@ -138,6 +135,43 @@ class WebWorkerManager {
           dY,
           juliaPoint,
         });
+        this.nextChunk += 1;
+      };
+
+      for (let i = 0; i < nThreadsFree; i += 1) {
+        const w = this.workers[i];
+        w.onmessage = (e) => {
+          if (e.data.id === roundID) {
+            try {
+              newArr.set(e.data.fractal.arr, e.data.offset);
+            } catch (err) {
+              // eslint-disable-next-line no-console
+              console.error(err);
+            }
+            this.remaining_chunks -= 1;
+            if (this.remaining_chunks === 0) {
+              this.arr = newArr;
+              res(
+                {
+                  arr: this.arr,
+                  height: this.height,
+                  width: this.width,
+                },
+              );
+            } else if (this.remaining_chunks >= nThreadsFree) {
+              renderChunk(
+                w,
+                Math.floor(this.nextChunk * this.pixelSplit),
+                Math.floor((this.nextChunk + 1) * this.pixelSplit),
+              );
+            }
+          }
+        };
+        renderChunk(
+          w,
+          Math.floor(this.nextChunk * this.pixelSplit),
+          Math.floor((this.nextChunk + 1) * this.pixelSplit),
+        );
       }
     });
   }
