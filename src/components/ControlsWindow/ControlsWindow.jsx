503b159709cac0e8464d9a132541d57f72bcd4fd
freddiejbawden <freddiejbawden@gmail.com>
2020-02-12 19:55:51 +0000

    Controls Mobile

--- src/components/ControlsWindow/ControlsWindow.jsx
+++ src/components/ControlsWindow/ControlsWindow.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-  Segment, Header, Icon, Transition,
+  Segment, Modal, Header, Icon, Button,
 } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
@@ -10,6 +10,14 @@ import { withStore } from '../../statemanagement/createStore';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class ControlsWindow extends Component {
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.store.controls !== this.controls) {
+      this.controls = nextProps.store.controls;
+      return true;
+    }
+    return false;
+  }
+
   handleClose() {
     const p = this.props;
     const st = p.store;
@@ -20,54 +28,74 @@ class ControlsWindow extends Component {
     let content;
     const p = this.props;
     const s = p.store;
-    if (s.controls) {
+    const controlsInfo = (
+      <div>
+        <div className="controls-top">
+          <Header>Controls</Header>
+          <Icon name="close" className="controls-close" onClick={(e) => this.handleClose(e)} />
+        </div>
+        <div className="details-container">
+          <div>
+            <Header as="h4">Keyboard</Header>
+            <div className="controls-container">
+              <Control keys={['arrow left', 'arrow right', 'arrow up', 'arrow down']} description="Pan Fractal" />
+              <Control keys={['Shift', 'Arrow']} description="Move Julia Pin" join />
+              <Control keys={['+', '-']} description="Zoom" />
+              <Control keys={['1']} description="Change focus to Mandelbrot" />
+              <Control keys={['2']} description="Change focus to Julia" />
+              <Control keys={['C']} description="Centre Julia Pin" />
+              <Control keys={['R']} description="Reset Fractals" />
+              <Control keys={['S']} description="Open Settings Menu" />
+              <Control keys={['H']} description="Open Help Menu" />
+            </div>
+          </div>
+          <div className="divider" />
+          <div>
+            <Header as="h4">Mouse</Header>
+            <div className="controls-container">
+              <Control type="text" text="Click + Drag" description={'Pan Fractal /\n Move Julia Pin'} />
+              <Control type="text" text="Scroll / Pinch" description="Zoom Fractal" />
+            </div>
+            <Header as="h4">Touch</Header>
+            <div className="controls-container">
+              <Control type="text" text="Touch + Drag" description="Pan Fractal" />
+              <Control type="text" text="Pinch / Double Tap" description="Zoom Fractal" />
+            </div>
+          </div>
+        </div>
+        <Button
+          primary
+          className="controls-close-button"
+          onClick={() => this.handleClose()}
+        >
+    Close
+        </Button>
+      </div>
+    );
+    console.log(window.innerWidth);
+    if (s.controls && window.innerWidth < 600) {
+      content = (
+        <Modal
+          className="controls-modal"
+          open
+        >
+          {controlsInfo}
+        </Modal>
+      );
+    } else if (s.controls) {
       content = (
         <Segment
+          open
           className="controls-window"
-          style={{
-            position: 'fixed',
-          }}
         >
-          <div className="controls-top">
-            <Header>Controls</Header>
-            <Icon name="close" className="controls-close" onClick={(e) => this.handleClose(e)} />
-          </div>
-          <div className="details-container">
-            <div>
-              <Header as="h4">Keyboard</Header>
-              <div className="controls-container">
-                <Control keys={['arrow left', 'arrow right', 'arrow up', 'arrow down']} description="Pan Fractal" />
-                <Control keys={['Shift', 'Arrow']} description="Move Julia Pin" join />
-                <Control keys={['+', '-']} description="Zoom" />
-                <Control keys={['1']} description="Change focus to Mandelbrot" />
-                <Control keys={['2']} description="Change focus to Julia" />
-                <Control keys={['C']} description="Centre Julia Pin" />
-                <Control keys={['R']} description="Reset Fractals" />
-                <Control keys={['S']} description="Open Settings Menu" />
-                <Control keys={['H']} description="Open Help Menu" />
-              </div>
-            </div>
-            <div className="divider" />
-            <div>
-              <Header as="h4">Mouse</Header>
-              <div className="controls-container">
-                <Control type="text" text="Click + Drag" description={'Pan Fractal /\n Move Julia Pin'} />
-                <Control type="text" text="Scroll / Pinch" description="Zoom Fractal" />
-              </div>
-              <Header as="h4">Touch</Header>
-              <div className="controls-container">
-                <Control type="text" text="Touch + Drag" description="Pan Fractal" />
-                <Control type="text" text="Pinch / Double Tap" description="Zoom Fractal" />
-              </div>
-            </div>
-          </div>
+          {controlsInfo}
         </Segment>
       );
     }
     return (
-      <Transition.Group animation="fly right" duration={500}>
+      <div>
         {content}
-      </Transition.Group>
+      </div>
     );
   }
 }
