718c1db908dc9636cbb8ca6bf014bcc4df28443a
freddiejbawden <freddiejbawden@gmail.com>
2020-03-23 14:08:57 +0000

    Updated Embed

--- src/static/Feedback/Feedback.jsx
+++ src/static/Feedback/Feedback.jsx
@@ -21,8 +21,7 @@ function Feedback() {
             <a href="Please read the information sheet available here before beginning this survey: https://github.com/freddiejbawden/mandelbrotmaps/blob/develop/information_sheet_form.pdf">information sheet.</a>
           </i>
         </p>
-        <iframe title="feedback-form" src="https://docs.google.com/forms/d/e/1FAIpQLSeeWnYqnnegTZPjL2TwdapJYn5YZzcNPD9xFCEGThP_bFSS2A/viewform?embedded=true" scrolling="no" style={{ maxWidth: 640, width: '100%' }} height="2661" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
-
+        <iframe title="feedback-form" src="https://docs.google.com/forms/d/e/1FAIpQLSeeWnYqnnegTZPjL2TwdapJYn5YZzcNPD9xFCEGThP_bFSS2A/viewform?embedded=true" width="640" height="2645" style={{ maxWidth: 640, width: '100%' }} frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
       </div>
       {' '}
     </div>
